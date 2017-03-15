d4d14082d019b0b7e994d05f0c96f66447047f6f
aaneja <anant.aneja@gmail.com>
2017-03-15 02:05:39 -0700

    - Added a RecyclerView + Adapter - Tested with a basic query from the Landing Activity

--- app/src/main/java/com/codepath/aaneja/nytsearch/LandingActivity.java
+++ app/src/main/java/com/codepath/aaneja/nytsearch/LandingActivity.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.codepath.aaneja.nytsearch.adapters.DocAdapter;
 import com.codepath.aaneja.nytsearch.models.Doc;
 import com.codepath.aaneja.nytsearch.services.NYTArticleSearch;
 import com.google.gson.Gson;
@@ -22,22 +25,28 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
-        AsyncTask<Context, Integer, Integer> asyncTask = new AsyncTask<Context, Integer, Integer>() {
+        AsyncTask<Context, Integer, DocAdapter> asyncTask = new AsyncTask<Context, Integer, DocAdapter>() {
             @Override
-            protected Integer doInBackground(Context... params) {
+            protected DocAdapter doInBackground(Context... params) {
                 NYTArticleSearch nytArticleSearch = new NYTArticleSearch();
-
+                List<Doc> searchedDocs = null;
                 try {
-                    List<Doc> allDocs = nytArticleSearch.GetArticles("india");
-                    Log.i("FETCHED SUCCESFULLY", String.valueOf(allDocs.size()));
-                    final Gson gson = new Gson();
-                    Log.i("index 1", gson.toJson(allDocs));
+                    searchedDocs = nytArticleSearch.GetArticles("india");
+                    Log.i("FETCHED SUCCESFULLY", String.valueOf(searchedDocs.size()));
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                return 1;
+
+                return new DocAdapter(searchedDocs);
+            }
+
+            @Override
+            protected void onPostExecute(DocAdapter docAdapter) {
+                RecyclerView rvArticles = (RecyclerView) findViewById(R.id.rvArticles);
+                rvArticles.setAdapter(docAdapter);
+                rvArticles.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
             }
         };
         asyncTask.execute();
---
+++ app/src/main/java/com/codepath/aaneja/nytsearch/adapters/DocAdapter.java
@@ -0,0 +1,72 @@
+package com.codepath.aaneja.nytsearch.adapters;
+
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.aaneja.nytsearch.R;
+import com.codepath.aaneja.nytsearch.models.Doc;
+
+import java.util.List;
+
+/**
+ * Created by aaneja on 15/03/17.
+ */
+
+public class DocAdapter extends  RecyclerView.Adapter<DocAdapter.ViewHolder> {
+
+    private List<Doc> FetchedArticles;
+
+    public DocAdapter(List<Doc> fetchedArticles) {
+        FetchedArticles = fetchedArticles;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View articleView = inflater.inflate(R.layout.layout_article, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(articleView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Doc doc = FetchedArticles.get(position);
+
+        holder.snippet.setText(doc.snippet);
+        //set thumbnail here too
+    }
+
+    @Override
+    public int getItemCount() {
+        return FetchedArticles.size();
+    }
+
+    public class ViewHolder  extends RecyclerView.ViewHolder {
+
+        public ImageView thumbnail;
+        private TextView snippet;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            thumbnail = (ImageView) itemView.findViewById(R.id.ivThumbnail);
+            snippet = (TextView) itemView.findViewById(R.id.tvSnippet);
+
+        }
+
+    }
+
+
+}
