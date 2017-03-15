a603dfe1e77a6ff944464169721f7990e00d54d4
aaneja <anant.aneja@gmail.com>
2017-03-15 03:05:06 -0700

    - Redid the asyncTask to be a updateSearchedDocsTask. Now we can keep populating fresh results on search just by using the docAdapter - Tested with landing page first load

--- app/src/main/java/com/codepath/aaneja/nytsearch/LandingActivity.java
+++ app/src/main/java/com/codepath/aaneja/nytsearch/LandingActivity.java
@@ -1,10 +1,8 @@
 package com.codepath.aaneja.nytsearch;
 
-import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
@@ -13,56 +11,63 @@ import com.codepath.aaneja.nytsearch.adapters.DocAdapter;
 import com.codepath.aaneja.nytsearch.models.Doc;
 import com.codepath.aaneja.nytsearch.models.SearchParams;
 import com.codepath.aaneja.nytsearch.services.NYTArticleSearch;
-import com.google.gson.Gson;
 
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LandingActivity extends AppCompatActivity {
+
+    private List<Doc> searchedDocs = new ArrayList<>(10);
+    private DocAdapter docAdapter = new  DocAdapter(searchedDocs);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
-        AsyncTask<SearchParams, Integer, DocAdapter> asyncTask = new AsyncTask<SearchParams, Integer, DocAdapter>() {
+
+        AsyncTask<SearchParams, Integer, List<Doc>> updateSearchedDocsTask = new AsyncTask<SearchParams, Integer, List<Doc>>() {
 
             private final NYTArticleSearch nytArticleSearch = new NYTArticleSearch();
-            private final RecyclerView rvArticles = (RecyclerView) findViewById(R.id.rvArticles);
 
             @Override
-            protected DocAdapter doInBackground(SearchParams... searchItem) {
+            protected List<Doc> doInBackground(SearchParams... searchItem) {
 
                 if (searchItem.length != 1)
                 {
                     //throw new Exception("params must have exactly one item");
                 }
 
-                List<Doc> searchedDocs = null;
+                List searchedDocsNew = Collections.EMPTY_LIST;
                 try {
-                    searchedDocs = nytArticleSearch.GetArticles(searchItem[0]);
-                    Log.i("FETCHED SUCCESSFULLY", String.valueOf(searchedDocs.size()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
+                    searchedDocsNew = nytArticleSearch.GetArticles(searchItem[0]);
+                    Log.i("FETCHED SUCCESSFULLY", String.valueOf(searchedDocsNew.size()));
+                } catch (IOException | JSONException e) {
                     e.printStackTrace();
                 }
 
-                return new DocAdapter(searchedDocs);
+                return searchedDocsNew;
             }
 
             @Override
-            protected void onPostExecute(DocAdapter docAdapter) {
-                rvArticles.setAdapter(docAdapter);
-                rvArticles.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
+            protected void onPostExecute(List<Doc> newResults) {
+                searchedDocs.clear();
+                searchedDocs.addAll(newResults);
+                docAdapter.notifyDataSetChanged();
             }
         };
 
+        RecyclerView rvArticles = (RecyclerView) findViewById(R.id.rvArticles);
+        rvArticles.setAdapter(docAdapter);
+        rvArticles.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
+
         SearchParams searchParams = new SearchParams();
         searchParams.SearchTerm = "food";
-        asyncTask.execute(searchParams);
+        updateSearchedDocsTask.execute(searchParams);
 
     }
 }
